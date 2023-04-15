e39cc9696860afea5e1055b03b3ff68c68f885db
juansensio <sensio.juan@gmail.com>
2023-04-15 20:46:28 +0100

    update

--- api/main.py
+++ api/main.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import AutoTokenizer, GPTNeoXForCausalLM
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+print("Loading model...")
 checkpoint = "OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5"
 cache_dir = '/cache'
 tokenizer = AutoTokenizer.from_pretrained(checkpoint, cache_dir=cache_dir)
 model = GPTNeoXForCausalLM.from_pretrained(checkpoint, cache_dir=cache_dir)
-
+print("Model loaded.")
 
 class Body(BaseModel):
     prompt: str
@@ -17,4 +26,6 @@ class Body(BaseModel):
 def generate(body: Body):
     inputs = tokenizer(f"<|prompter|>{body.prompt}<|endoftext|><|assistant|>", return_tensors="pt")
     tokens = model.generate(**inputs)
-    return tokenizer.decode(tokens[0], skip_special_tokens=True)
+    return {
+        'response': tokenizer.decode(tokens[0]).split('<|assistant|>')[-1].split('<|endoftext|>')[0]
+    }
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,7 +1,56 @@
-<h1 class="text-3xl font-bold underline">Hello worlds!</h1>
+<script>
+  let prompt = "";
+  let prompts = [];
+  let responses = [];
+  let loading = false;
+  const generate = () => {
+    if (!prompt || prompt.length == 0) return;
+    prompts = [...prompts, prompt];
+    responses = [...responses, "Loading..."];
+    loading = true;
+    fetch("http://localhost:8000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        responses = [...responses.slice(0, -1), res.response];
+        loading = false;
+      })
+      .catch((err) => {
+        alert(err.message);
+        loading = false;
+        responses = [...responses.slice(0, -1), "Failed :("];
+      });
+    prompt = "";
+  };
+</script>
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
+<main class="p-3 flex flex-col w-full h-screen justify-between">
+  <div>
+    <h1 class="text-3xl font-bold">ChatGPT clone</h1>
+    <div class="overflow-auto">
+      {#each prompts as prompt, i}
+        <div class="p-2 rounded-lg">{prompt}</div>
+        <div class="bg-gray-200 p-2 rounded-lg">{responses[i]}</div>
+      {/each}
+    </div>
+  </div>
+  <form on:submit|preventDefault={generate} class="flex flex-row gap-3">
+    <input
+      type="text"
+      class="border border-2-gray p-2 w-full"
+      placeholder="Prompt..."
+      bind:value={prompt}
+    />
+    <button
+      disabled={loading}
+      type="submit"
+      class="border border-2-gray p-2 {loading ? 'bg-gray-200' : ''}}"
+      >Generate</button
+    >
+  </form>
+</main>
